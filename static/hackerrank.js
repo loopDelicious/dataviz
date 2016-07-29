92a1943c8012b8ef6331611fc395284a37e88525
Joyce Lin <joycelin79@gmail.com>
2016-07-28 18:46:38 -0700

    add success rate and multi-axis bar and line chart format

--- static/hackerrank.js
+++ static/hackerrank.js
@@ -1,6 +1,9 @@
+
+// pull in HRdata from data.js
 var days = [];
 var submissions = [];
 var topics = [];
+var success = [];
 
 function readData() {
 
@@ -8,41 +11,100 @@ function readData() {
         days.push(HRdata[i][0]);
         submissions.push(HRdata[i][1]);
         topics.push(HRdata[i][2]);
+        success.push(HRdata[i][3]);
     }
 }
 
 readData();
 
-//get chart canvas
-var ctx = document.getElementById("myChart");
+// set up chart data
+var myChartData = {
+    labels: topics,
+    datasets: [{
+        type: 'bar',
+        label: '# of submissions',
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        data: submissions,
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        yAxisID: "y-axis-1",
+    }, {
+        type: 'line',
+        label: 'success rate',
+        backgroundColor: "rgba(151,187,205,0.5)",
+        data: success,
+        borderColor: 'white',
+        borderWidth: 2,
+        yAxisID: "y-axis-2",
+    }, ]
+};
 
-// instantiate chart object
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: topics,
-        datasets: [
-            {
-            label: '# of submissions',
-            data: submissions,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
+// instantiate chart object, and plot data on html canvas
+window.onload = function() {
+    var ctx = document.getElementById("myChart");
+    window.myBar = new Chart(ctx, {
+        type: 'bar',
+        data: myChartData,
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: '30 days of coding with HackerRank'
+            },
+            scales: {
+                yAxes: [{
+                    type: "linear",
+                    display: true,
+                    position: "left",
+                    id: "y-axis-1",
+                }, {
+                    type: "linear",
+                    display: true,
+                    position: "right",
+                    id: "y-axis-2",
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }],
             }
-        ]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
         }
-    }
-});
+    });
+};
 
-// Example:
+// https://github.com/chartjs/Chart.js/blob/v2.0-dev/samples/combo-bar-line.html
+// https://github.com/chartjs/Chart.js/blob/v2.0-dev/samples/bar-multi-axis.html
+
+
+//Take I:
+//get chart canvas
+// var ctx = document.getElementById("myChart");
+// instantiate chart object
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: topics,
+//         datasets: [
+//             {
+//             label: '# of submissions',
+//             data: submissions,
+//             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//             borderColor: 'rgba(255,99,132,1)',
+//             borderWidth: 1,
+//             }
+//         ]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
+
+// Chart.JS Example:
 // var ctx = document.getElementById("myChart");
 // var myChart = new Chart(ctx, {
 //     type: 'bar',
